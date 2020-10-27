7a878ef9bec77ee9b864abb97bda36c64b1f54c8
lmodig93 <ludvigmodig@gmail.com>
2020-10-27 13:11:04 +0100

    Assignment2

---
+++ Day7/Assignment2/include/Shape.h
@@ -0,0 +1,42 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+#include <iostream>
+
+class Shape{
+    protected:
+        int width, height;
+    public:
+        void set_values(int a, int b);
+        void print_values();
+        bool operator<(Shape &shape);
+        bool operator>(Shape &shape);
+        bool operator==(Shape &shape);
+        virtual int area() = 0;
+        virtual int perimeter() = 0;
+};
+
+class Rectangle: public Shape{
+    public:
+        int area();
+        int perimeter();
+};
+
+class Triangle: public Shape{
+    public:
+        int area();
+        int perimeter();
+};
+
+class Square: public Shape{
+    public:
+        int area();
+        int perimeter();
+};
+
+class Circle: public Shape{
+    public:
+        int area();
+        int perimeter();
+};
+
+#endif
